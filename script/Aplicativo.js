310fc1a59bf2dfd814f1dbd3a3721f32bc6307ec
Hugo da Rocha Brandão <hugobrandao.dev@gmail.com>
2020-08-22 14:39:02 -0300

    Modificado: Aplicativo.js (para que haja a estilização com CSS da requisição quando a mesma for concluida)

--- script/Aplicativo.js
+++ script/Aplicativo.js
@@ -32,48 +32,51 @@ class Aplicativo {
 			requisitorBase.get(`${tipo}/${nome}`)
 				.then(response => {
 					if (tipo === 'users') {
-						var {name, avatar_url, html_url, bio, type} = response.data
+						var { name, avatar_url, html_url, bio, type } = response.data
 						this.renderizarRequisicao(name, avatar_url, html_url, type, bio)
 
 					} else if (tipo === 'repos') {
-						var {name, html_url, description, owner: {avatar_url, type}} = response.data
+						var { name, html_url, description, owner: { avatar_url, type } } = response.data
 						this.renderizarRequisicao(name, avatar_url, html_url, type, description)
 					}
 				})
 				.catch(error => {
+					alert('OPS: Não encontramos nenhum usuário ou repositório com esse nome/caminho.')
 					console.log(error)
 				})
 		}
 	}
-// name, avatar_url, html_url, type, bio
+	// name, avatar_url, html_url, type, bio
 	renderizarRequisicao(nome, foto, acessar, tipo, descricao) {
 		let $pElement_tipo = document.createElement('p')
-					$pElement_tipo.innerHTML = tipo
+		$pElement_tipo.innerHTML = tipo
 
-					let $h4Element_nome = document.createElement('h4')
-					$h4Element_nome.setAttribute('class', 'tipo')
-					$h4Element_nome.innerHTML = nome
+		let $h4Element_nome = document.createElement('h4')
+		$h4Element_nome.setAttribute('class', 'tipo')
+		$h4Element_nome.innerHTML = nome
 
-					let $imgElement_imagem = document.createElement('img')
-					$imgElement_imagem.setAttribute('src', foto)
-					$imgElement_imagem.setAttribute('alt', 'Foto do perfil')
-					$imgElement_imagem.setAttribute('class', 'imagem')
+		let $imgElement_imagem = document.createElement('img')
+		$imgElement_imagem.setAttribute('src', foto)
+		$imgElement_imagem.setAttribute('alt', 'Foto do perfil')
+		$imgElement_imagem.setAttribute('class', 'imagem')
 
-					let $pElement_descricao = document.createElement('p')
-					$pElement_descricao.innerHTML = descricao
+		let $pElement_descricao = document.createElement('p')
+		$pElement_descricao.innerHTML = descricao
 
-					let $aElement_link = document.createElement('a')
-					$aElement_link.setAttribute('href', acessar)
-					$aElement_link.setAttribute('class', 'link-acesso')
-					$aElement_link.innerHTML = 'Quero conhecer <i class="fas fa-arrow-circle-right"></i>'
+		let $aElement_link = document.createElement('a')
+		$aElement_link.setAttribute('href', acessar)
+		$aElement_link.setAttribute('class', 'link-acesso')
+		$aElement_link.setAttribute('target', '_blank')
+		$aElement_link.innerHTML = 'Quero conhecer <i class="fas fa-arrow-circle-right"></i>'
 
-					let articleElement_requisicao = document.createElement('article')
-					articleElement_requisicao.appendChild($pElement_tipo)
-					articleElement_requisicao.appendChild($h4Element_nome)
-					articleElement_requisicao.appendChild($imgElement_imagem)
-					articleElement_requisicao.appendChild($pElement_descricao)
-					articleElement_requisicao.appendChild($aElement_link)
+		let $articleElement_requisicao = document.createElement('article')
+		$articleElement_requisicao.setAttribute('class', 'requisicao')
+		$articleElement_requisicao.appendChild($pElement_tipo)
+		$articleElement_requisicao.appendChild($h4Element_nome)
+		$articleElement_requisicao.appendChild($imgElement_imagem)
+		$articleElement_requisicao.appendChild($pElement_descricao)
+		$articleElement_requisicao.appendChild($aElement_link)
 
-					this.sectionElement_requisicoes.appendChild(articleElement_requisicao)
+		this.sectionElement_requisicoes.appendChild($articleElement_requisicao)
 	}
 }
